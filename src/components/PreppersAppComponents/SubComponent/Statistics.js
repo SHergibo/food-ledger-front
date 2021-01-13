e0b6afa0f9f51df4935a588e08d9b80a81958273
SHergibo <sachau888@hotmail.com>
2021-01-13 14:29:33 +0100

    feat: Added color to charts.

--- src/components/PreppersAppComponents/SubComponent/Statistics.js
+++ src/components/PreppersAppComponents/SubComponent/Statistics.js
@@ -39,6 +39,9 @@ function Statistics() {
       {
         label: 'N° de produit périmé',
         data: dataChartOne,
+        backgroundColor: 'rgba(3, 62, 129, 0.5)',
+        borderColor: 'rgba(3, 62, 129, 1)',
+        borderWidth: 1,
       },
     ],
   };
@@ -61,6 +64,31 @@ function Statistics() {
     datasets: [
       {
         data: dataChartTwo,
+        backgroundColor: [
+          'rgba(255, 99, 132, 0.2)',
+          'rgba(54, 162, 235, 0.2)',
+          'rgba(255, 206, 86, 0.2)',
+          'rgba(75, 192, 192, 0.2)',
+          'rgba(153, 102, 255, 0.2)',
+          'rgba(255, 159, 64, 0.2)',
+          'rgba(0, 128, 0, 0.2)',
+          'rgba(255, 255, 0, 0.2)',
+          'rgba(128, 0, 128, 0.2)',
+          'rgba(128, 0, 0, 0.2)',
+        ],
+        borderColor: [
+          'rgba(255, 99, 132, 1)',
+          'rgba(54, 162, 235, 1)',
+          'rgba(255, 206, 86, 1)',
+          'rgba(75, 192, 192, 1)',
+          'rgba(153, 102, 255, 1)',
+          'rgba(255, 159, 64, 1)',
+          'rgba(0, 128, 0, 1)',
+          'rgba(255, 255, 0, 1)',
+          'rgba(128, 0, 128, 1)',
+          'rgba(128, 0, 0, 1)',
+        ],
+        borderWidth: 1,
       },
     ],
   };
@@ -70,6 +98,31 @@ function Statistics() {
     datasets: [
       {
         data: dataChartThree,
+        backgroundColor: [
+          'rgba(255, 99, 132, 0.2)',
+          'rgba(54, 162, 235, 0.2)',
+          'rgba(255, 206, 86, 0.2)',
+          'rgba(75, 192, 192, 0.2)',
+          'rgba(153, 102, 255, 0.2)',
+          'rgba(255, 159, 64, 0.2)',
+          'rgba(0, 128, 0, 0.2)',
+          'rgba(255, 255, 0, 0.2)',
+          'rgba(128, 0, 128, 0.2)',
+          'rgba(128, 0, 0, 0.2)',
+        ],
+        borderColor: [
+          'rgba(255, 99, 132, 1)',
+          'rgba(54, 162, 235, 1)',
+          'rgba(255, 206, 86, 1)',
+          'rgba(75, 192, 192, 1)',
+          'rgba(153, 102, 255, 1)',
+          'rgba(255, 159, 64, 1)',
+          'rgba(0, 128, 0, 1)',
+          'rgba(255, 255, 0, 1)',
+          'rgba(128, 0, 128, 1)',
+          'rgba(128, 0, 0, 1)',
+        ],
+        borderWidth: 1,
       },
     ],
   };
