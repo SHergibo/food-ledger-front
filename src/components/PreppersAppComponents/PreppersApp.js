ba2645b720890e1fa50b02741df7b842d369625b
SHergibo <sachau888@hotmail.com>
2021-02-03 15:46:45 +0100

    fix: Optimised Some useState rendering in PreppersApp and SubContainer componenent.

--- src/components/PreppersAppComponents/PreppersApp.js
+++ src/components/PreppersAppComponents/PreppersApp.js
@@ -1,4 +1,4 @@
-import React, { useCallback, useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { DataProvider } from './DataContext';
 import { logout, refreshToken } from './../../utils/Auth';
 import Nav from './PreppersAppUI/Nav';
@@ -9,30 +9,7 @@ import SubContainer from './PreppersAppUI/SubContainer';
 import PropTypes from 'prop-types';
 
 function PreppersApp({ history }) {
-  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
   const [showNotification, setShowNotification] = useState(false);
-
-  const responsive = useCallback(() => {
-    setWindowWidth(window.innerWidth);
-  }, []);
-
-  useEffect(() => {
-    window.addEventListener('resize', responsive);
-    return () => {
-      window.removeEventListener('resize', responsive);
-    }
-  }, [responsive]);
-
-  //TODO créer une route comme protected.route pour gérer la route notification hors responsive mobile
-  useEffect(() => {
-      if(windowWidth > 640 && history.location.pathname === "/app/notification" && history.length <= 2){
-        history.push({
-          pathname: '/app/liste-produit',
-        })
-      }else if (windowWidth > 640 && history.location.pathname === "/app/notification" && history.length > 2){
-        history.goBack();
-      }
-  }, [windowWidth, history]);
 
   useEffect(() => {
     const refreshTokenInterval = setInterval(() => {
--- src/components/PreppersAppComponents/PreppersAppUI/SubContainer.js
+++ src/components/PreppersAppComponents/PreppersAppUI/SubContainer.js
@@ -1,10 +1,34 @@
-import React from 'react';
+import React, {useEffect, useState, useCallback} from 'react';
+import { withRouter } from "react-router-dom";
 import { useNotificationData } from './../DataContext';
 import axiosInstance from '../../../utils/axiosInstance';
 import { apiDomain, apiVersion } from '../../../apiConfig/ApiConfig';
 
-function SubContainer() {
+function SubContainer({history}) {
   const { notification, setNotification } = useNotificationData();
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+
+  const responsive = useCallback(() => {
+    setWindowWidth(window.innerWidth);
+  }, []);
+
+  useEffect(() => {
+    window.addEventListener('resize', responsive);
+    return () => {
+      window.removeEventListener('resize', responsive);
+    }
+  }, [responsive]);
+
+  //TODO créer une route comme protected.route pour gérer la route notification hors responsive mobile
+  useEffect(() => {
+      if(windowWidth > 640 && history.location.pathname === "/app/notification" && history.length <= 2){
+        history.push({
+          pathname: '/app/liste-produit',
+        })
+      }else if (windowWidth > 640 && history.location.pathname === "/app/notification" && history.length > 2){
+        history.goBack();
+      }
+  }, [windowWidth, history]);
 
   const interactionNotification = async (urlRequest, id, accepted) => {
     const requestNotificationEndPoint = `${apiDomain}/api/${apiVersion}/requests/${urlRequest}/${id}?acceptedRequest=${accepted}`;
@@ -37,4 +61,4 @@ function SubContainer() {
   )
 }
 
-export default SubContainer
+export default withRouter(SubContainer)
