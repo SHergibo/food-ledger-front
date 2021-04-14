afb247c86940436806a8b55d51c8baa082bbde8f
SHergibo <sachau888@hotmail.com>
2021-04-14 16:39:56 +0200

    feat: Created CreateHouseholdForm component.

---
+++ src/components/PreppersAppComponents/SubComponent/ProfileComponents/CreateHouseholdForm.js
@@ -0,0 +1,73 @@
+import React, { useState, useRef } from 'react';
+import { useUserData, useUserHouseHoldData } from '../../DataContext';
+import { useForm } from 'react-hook-form';
+import axiosInstance from '../../../../utils/axiosInstance';
+import { apiDomain, apiVersion } from '../../../../apiConfig/ApiConfig';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import InformationIcon from '../../UtilitiesComponent/InformationIcons';
+
+function CreateHouseholdForm() {
+  const { setUserHouseholdData } = useUserHouseHoldData();
+  const { setUserData } = useUserData();
+  const [ errorMessage, setErrorMessage ] = useState(false);
+  const [ messageError, setMessageError ] = useState("");
+  const isMounted = useRef(true);
+
+  const { register, handleSubmit, errors } = useForm({
+    mode: "onChange"
+  });
+
+  const createdHousehold = async (data) => {
+    
+    const createHouseholdEndpoint = `${apiDomain}/api/${apiVersion}/households`;
+
+    await axiosInstance.post(createHouseholdEndpoint, data)
+      .then((response) => {
+        if(response.status === 200 && isMounted.current){
+          setUserHouseholdData(response.data.householdData);
+          setUserData(response.data.userData);
+          setErrorMessage(false);
+          setMessageError("");
+        }
+      })
+      .catch((error) => {
+        if(isMounted.current){
+          setErrorMessage(true);
+          setMessageError(error.response.data.output.payload.message);
+        }
+      });
+  };
+
+  const clearErrorMessage = () =>{
+    if(errorMessage){
+      setErrorMessage(false);
+    }
+  }
+
+
+  return (
+    <form className="form-inline" onSubmit={handleSubmit(createdHousehold)}>
+      <div className="input-form-container-with-error">
+        <label htmlFor="householdName">Créer une famille *</label>
+        <input name="householdName" className="input-form" type="text" id="householdName" placeholder="Nom de la famille..." onChange={clearErrorMessage} ref={register({ required : true })} />
+        {errors.householdName && <span className="error-message-form">Ce champ est requis</span>}
+      </div>
+      <div className="default-action-form-container">
+        <button 
+        className={"default-btn-action-form"}
+        type="submit">
+          <FontAwesomeIcon icon="plus" /> Créer
+        </button> 
+        {errorMessage &&
+          <InformationIcon 
+            className="error-icon"
+            icon={<FontAwesomeIcon icon="times" />}
+            message={messageError}
+          />
+        }
+      </div>
+    </form>
+  )
+}
+
+export default CreateHouseholdForm;
