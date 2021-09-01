6c3673d119c554052ca69575ba1b4079a2f9866c
SHergibo <sachau888@hotmail.com>
2021-09-01 16:00:15 +0200

    feat: Used Table component instead of using a table directly in BrandOption component.

--- src/components/PreppersAppComponents/SubComponent/OptionComponents/BrandOption.js
+++ src/components/PreppersAppComponents/SubComponent/OptionComponents/BrandOption.js
@@ -3,14 +3,20 @@ import { Link } from 'react-router-dom';
 import axiosInstance from '../../../../utils/axiosInstance';
 import { apiDomain, apiVersion } from '../../../../apiConfig/ApiConfig';
 import { useUserHouseHoldData, useSocket, useUserData } from '../../DataContext';
+import Table from './../../UtilitiesComponent/Table';
+import { columnsBrandOption } from "./../../../../utils/localData";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 function BrandOption() {
   const [brands, setBrands] = useState([]);
+  const [hasBrand, setHasBrand] = useState(false);
+  const [pageIndex, setPageIndex] = useState(1);
+  const [pageCount, setPageCount] = useState(0);
   const isMounted = useRef(true);
   const { userHouseholdData } = useUserHouseHoldData();
   const { userData } = useUserData();
   const { socketRef } = useSocket();
+  const pageSize = 12;
 
   useEffect(() => {
     let socket = null;
@@ -95,18 +101,22 @@ function BrandOption() {
 
   useEffect(() => {
     const getBrand = async () => {
-      const getBrandEndPoint = `${apiDomain}/api/${apiVersion}/brands/find-all/${userHouseholdData._id}`;
+      const getBrandEndPoint = `${apiDomain}/api/${apiVersion}/brands/pagination/${userHouseholdData._id}?page=${pageIndex - 1}`;
       await axiosInstance.get(getBrandEndPoint)
         .then(async (response) => {
           if(isMounted.current){
-            setBrands(response.data);
+            setBrands(response.data.arrayData);
+            setPageCount(Math.ceil(response.data.totalBrand / pageSize));
+            setHasBrand(true);
+          }else{
+            setHasBrand(false);
           }
         });
     };
     if(userHouseholdData){
       getBrand();
     }
-  }, [userHouseholdData]);
+  }, [userHouseholdData, pageIndex]);
 
   useEffect(() => {
     return () => {
@@ -115,103 +125,121 @@ function BrandOption() {
   }, []);
 
   const deleteBrand = async (brandId) => {
-    const removeBrandEndpoint = `${apiDomain}/api/${apiVersion}/brands/${brandId}`;
+    if(brands.length === 1 && pageIndex > 1){
+      setPageIndex(currPageIndex => currPageIndex - 1);
+    }
+
+    const removeBrandEndpoint = `${apiDomain}/api/${apiVersion}/brands/delete-pagination/${brandId}?page=${pageIndex - 1}`;
     await axiosInstance.delete(removeBrandEndpoint)
       .then((response)=> {
-        setBrands(brands => brands.filter((brand) => brand._id !== response.data._id));
+        setBrands(response.data.arrayData);
+        setPageCount(Math.ceil(response.data.totalBrand / pageSize));
+        if(response.data.totalBrand >= 1){
+          setHasBrand(true);
+        }else{
+          setHasBrand(false);
+        }
       });
   };
 
-  let tableBrand = <div className="option-component">
-    <h2 className="default-h2">Tableau des marques</h2>
-    <div className="container-list-table list-table-profile">
-      <table className="list-table">
-        <thead className="thead-no-cursor">
-          <tr>
-            <th>Nom</th>
-            <th>N° de produit lié</th>
-            <th>N° d'historique lié</th>
-            <th>Action</th>
-          </tr>
-        </thead>
-        <tbody>
-          {brands.map((brand, index) => {
+  let trTable = brands.map((row, indexRow) => {
+    return (
+      <tr key={`${row}-${indexRow}`}>
+        {columnsBrandOption.map((column, index) => {
+          if (column.id === 'action') {
             return (
-              <tr key={`brand-${index}`}>
-                <td className="td-align-center">
-                  {brand.brandName.label}
-                </td>
-                <td className="td-align-center">
-                  {brand.numberOfProduct}
-                </td>
-                <td className="td-align-center">
-                  {brand.numberOfHistoric}
-                </td>
-                <td>
-                  <div className="div-list-table-action">
-                    {userHouseholdData?.isWaiting ?
+              <td key={`${column.id}-${index}`}>
+                <div className="div-list-table-action">
+                  {userHouseholdData?.isWaiting ?
+                    <>
+                      <button title="Vous ne pouvez pas editer une marque si votre famille n'a pas d'administrateur.trice!" type="button" className="list-table-action-disabled" disabled><FontAwesomeIcon icon="edit"/></button>
+                      <button title="Vous ne pouvez pas supprimer une marque si votre famille n'a pas d'administrateur.trice!" type="button" className="list-table-action-disabled" disabled><FontAwesomeIcon icon="trash"/></button>
+                    </> :
+                    <> 
+                    {row.numberOfProduct >= 1 || row.numberOfHistoric >= 1 ? 
                       <>
-                        <button title="Vous ne pouvez pas editer une marque si votre famille n'a pas d'administrateur.trice!" type="button" className="list-table-action-disabled" disabled><FontAwesomeIcon icon="edit"/></button>
-                        <button title="Vous ne pouvez pas supprimer une marque si votre famille n'a pas d'administrateur.trice!" type="button" className="list-table-action-disabled" disabled><FontAwesomeIcon icon="trash"/></button>
-                      </> :
-                      <> 
-                      {brand.numberOfProduct >= 1 || brand.numberOfHistoric >= 1 ? 
+                      {row.isBeingEdited ?
                         <>
-                        {brand.isBeingEdited ?
-                          <>
-                            {userData.role === "admin" ?
-                              <Link title="Attention, une autre personne édite cette marque, si vous cliquez sur ce bouton, la personne sera éjectée du formulaire d'édition et perdra toutes ses données!" className="list-table-action-warning" to={`/app/edition-marque/${brand._id}`} ><FontAwesomeIcon icon="edit" /></Link>
-                            :
-                              <button title="Une autre personne édite ce produit!" className="list-table-action-disabled" disabled><FontAwesomeIcon icon="edit" /></button>
-                            }
-                          </>
-                        :
-                          <Link title="Éditer la marque" className="list-table-action" to={`/app/edition-marque/${brand._id}`}><FontAwesomeIcon icon="edit" /></Link>
-                        }
-                          <button title="Vous ne pouvez pas supprimer une marque si elle est utilisée dans un produit ou historique!" type="button" className="list-table-action-disabled" disabled><FontAwesomeIcon icon="trash"/></button>
-                        </>
-                        :
-                        <>
-                        {brands.isBeingEdited ?
-                          <>
                           {userData.role === "admin" ?
-                            <>
-                              <Link title="Attention, une autre personne édite cette marque, si vous cliquez sur ce bouton, la personne sera éjectée du formulaire d'édition et perdra toutes ses données!" className="list-table-action-warning" to={`/app/edition-marque/${brand._id}`} ><FontAwesomeIcon icon="edit" /></Link>
-                              <button title="Vous ne pouvez pas supprimer cette marque pendant qu'une personne l'édite!" className="list-table-action-disabled" disabled><FontAwesomeIcon icon="trash" /></button>
-                            </>
+                            <Link title="Attention, une autre personne édite cette marque, si vous cliquez sur ce bouton, la personne sera éjectée du formulaire d'édition et perdra toutes ses données!" className="list-table-action-warning" to={`/app/edition-marque/${row._id}`} ><FontAwesomeIcon icon="edit" /></Link>
                           :
-                          <>
                             <button title="Une autre personne édite ce produit!" className="list-table-action-disabled" disabled><FontAwesomeIcon icon="edit" /></button>
+                          }
+                        </>
+                      :
+                        <Link title="Éditer la marque" className="list-table-action" to={`/app/edition-marque/${row._id}`}><FontAwesomeIcon icon="edit" /></Link>
+                      }
+                        <button title="Vous ne pouvez pas supprimer une marque si elle est utilisée dans un produit ou historique!" type="button" className="list-table-action-disabled" disabled><FontAwesomeIcon icon="trash"/></button>
+                      </>
+                      :
+                      <>
+                      {brands.isBeingEdited ?
+                        <>
+                        {userData.role === "admin" ?
+                          <>
+                            <Link title="Attention, une autre personne édite cette marque, si vous cliquez sur ce bouton, la personne sera éjectée du formulaire d'édition et perdra toutes ses données!" className="list-table-action-warning" to={`/app/edition-marque/${row._id}`} ><FontAwesomeIcon icon="edit" /></Link>
                             <button title="Vous ne pouvez pas supprimer cette marque pendant qu'une personne l'édite!" className="list-table-action-disabled" disabled><FontAwesomeIcon icon="trash" /></button>
                           </>
-                          }
-                            
-                          </>
                         :
-                          <>
-                            <Link title="Éditer la marque" className="list-table-action" to={`/app/edition-marque/${brand._id}`}><FontAwesomeIcon icon="edit" /></Link>
-                            <button title="Supprimer la marque" type="button" className="list-table-action" onClick={()=>{deleteBrand(brand._id)}}><FontAwesomeIcon icon="trash"/></button>
-                          </>
+                        <>
+                          <button title="Une autre personne édite ce produit!" className="list-table-action-disabled" disabled><FontAwesomeIcon icon="edit" /></button>
+                          <button title="Vous ne pouvez pas supprimer cette marque pendant qu'une personne l'édite!" className="list-table-action-disabled" disabled><FontAwesomeIcon icon="trash" /></button>
+                        </>
                         }
                           
                         </>
+                      :
+                        <>
+                          <Link title="Éditer la marque" className="list-table-action" to={`/app/edition-marque/${row._id}`}><FontAwesomeIcon icon="edit" /></Link>
+                          <button title="Supprimer la marque" type="button" className="list-table-action" onClick={()=>{deleteBrand(row._id)}}><FontAwesomeIcon icon="trash"/></button>
+                        </>
                       }
+                        
                       </>
                     }
-                  </div>
-                </td>
-              </tr>  
+                    </>
+                  }
+                </div>
+              </td>
             )
-          })}
-        </tbody>
-      </table>
-    </div>     
-  </div>;
+          }
+          if (column.id === "brandName") {
+            return (
+              <td key={`${column.id}-${index}`} className="td-align-center">
+                {row[column.id].label}
+              </td>
+            )
+          }else{
+            return (
+            <td key={`${column.id}-${index}`} className="td-align-center">
+              {row[column.id]}
+            </td>
+            )
+          }
+        })}
+      </tr>
+    )
+  });
 
   return (
-    <>
-      {brands && <>{tableBrand}</>}
-    </>
+    <div className="container-brand">
+      <div className="option-component">
+        {!hasBrand &&
+          <div className="no-data-option">
+            <p>Pas de marque disponible !</p>
+          </div>
+        }
+        
+        {hasBrand &&
+          <Table 
+            columns={columnsBrandOption}
+            trTable={trTable}
+            pagination={true}
+            paginationInfo={{pageIndex, setPageIndex, pageCount}}
+          />
+        }
+      </div>
+    </div>
   )
 }
 
