e98524f06685c4aeb47c6b5fde5cf1747121f73b
SHergibo <sachau888@hotmail.com>
2021-09-21 15:13:00 +0200

    fix: Fixed bug in inputPagination function by using paginationInput useState instead if pageIndex Table component.

--- src/components/PreppersAppComponents/UtilitiesComponent/Table.js
+++ src/components/PreppersAppComponents/UtilitiesComponent/Table.js
@@ -1,9 +1,16 @@
-import React, { useCallback} from 'react';
+import React, { useCallback, useState, useEffect } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import PropTypes from 'prop-types';
 
 function Table({ columns, customTableClass, sorting, btnSortRef, sortObject, populateSortObject, setUrlPageQueryParam, trTable, pagination, paginationInfo, goToPageUrl }) {
   const { pageIndex, setPageIndex, pageCount } = paginationInfo;
+  const [ paginationInput, setPaginationInput ] = useState(1);
+
+  useEffect(() => {
+    if(pageIndex) {
+      setPaginationInput(pageIndex);
+    }
+  }, [pageIndex])
 
   let btnSortLogic = useCallback((btnSort, index) => {
     let svgSort = <svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="sort" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512"><path fill="currentColor" d="M41 288h238c21.4 0 32.1 25.9 17 41L177 448c-9.4 9.4-24.6 9.4-33.9 0L24 329c-15.1-15.1-4.4-41 17-41zm255-105L177 64c-9.4-9.4-24.6-9.4-33.9 0L24 183c-15.1 15.1-4.4 41 17 41h238c21.4 0 32.1-25.9 17-41z"></path></svg>;
@@ -55,10 +62,10 @@ function Table({ columns, customTableClass, sorting, btnSortRef, sortObject, pop
   const inputPagination = (e) => {
     if(e.target.value > pageCount){
       setPageIndex(pageCount);
+      setPaginationInput(pageCount);
       if(setUrlPageQueryParam) setUrlPageQueryParam(pageCount);
     } else if (e.target.value <= 0 || e.target.value === ""){
-      setPageIndex("");
-      if(setUrlPageQueryParam) setUrlPageQueryParam("");
+      setPaginationInput(null);
     } else {
       setPageIndex(e.target.value);
       if(setUrlPageQueryParam) setUrlPageQueryParam(e.target.value);
@@ -112,7 +119,7 @@ function Table({ columns, customTableClass, sorting, btnSortRef, sortObject, pop
               <span>Page
                 <input 
                 type="number" 
-                value={paginationInfo.pageIndex}
+                value={paginationInput}
                 min={1}
                 max={paginationInfo.pageCount}
                 onChange={(e) => {inputPagination(e)}}/>
