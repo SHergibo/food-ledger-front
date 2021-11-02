ae83f24b428fd54a810041aa25b071d66a0894dd
SHergibo <sachau888@hotmail.com>
2021-11-02 13:58:27 +0100

    feat: Created BackToTop component in UtilitiesComponent folder.

---
+++ src/components/PreppersAppComponents/UtilitiesComponent/BackToTop.js
@@ -0,0 +1,40 @@
+import React, { useEffect, useState } from "react";
+import { animateScroll as scroll } from 'react-scroll';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
+function BackToTop() {
+  const [show, setShow] = useState(false);
+
+  const handleBackToTopScroll= () => {
+    let scrollpage = Math.round(window.scrollY);
+    let windowHeight = window.innerHeight;
+    if (scrollpage > windowHeight/3) {
+      setShow(true);
+    } else {
+      setShow(false);
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener('scroll', handleBackToTopScroll);
+    return () => {
+      window.removeEventListener('scroll', handleBackToTopScroll);
+    }
+  }, []);
+  
+  const scrollToTop = () =>{
+    scroll.scrollToTop();
+  }
+  
+  return (
+    <>
+      {show && 
+        <div tabIndex={0} className="back-to-top" onClick={scrollToTop} onKeyPress={scrollToTop}>
+          <FontAwesomeIcon icon="chevron-up" />
+        </div>
+      }
+    </>
+  );
+}
+
+export default BackToTop;
