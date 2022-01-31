14f26c05360434ed089fc1308aaed8fbb31faf42
SHergibo <sachau888@hotmail.com>
2022-01-31 15:36:23 +0100

    fix: Changed logoutAndRefresh requestUrl to logout-and-refresh in logout function in Auth.js.

--- src/utils/Auth.js
+++ src/utils/Auth.js
@@ -1,59 +1,63 @@
-import Axios from 'axios';
-import {apiDomain, apiVersion} from './../apiConfig/ApiConfig';
+import Axios from "axios";
+import { apiDomain, apiVersion } from "./../apiConfig/ApiConfig";
 
 let response;
 let authenticated = false;
 
-const loginIn = async (data) =>{
+const loginIn = async (data) => {
   await Axios.post(`${apiDomain}/api/${apiVersion}/auth/login`, data)
-  .then((res) =>{
-    if(res.status === 200 && res.data.token.accessToken && res.data.token.expiresIn){
-      response = res.status;
-      let accessToken = res.data.token.accessToken;
-      let refresh_token = res.data.token.refreshToken.token;
-      let user_id = res.data.user._id;
-      let user_email = res.data.user.email;
-  
-      localStorage.setItem("access_token", accessToken);
-      localStorage.setItem('refresh_token', refresh_token);
-      localStorage.setItem('user_id', user_id);
-      localStorage.setItem('user_email', user_email);
-    }
-  })
-  .catch(() =>{
-    response = 401;
-  })
+    .then((res) => {
+      if (
+        res.status === 200 &&
+        res.data.token.accessToken &&
+        res.data.token.expiresIn
+      ) {
+        response = res.status;
+        let accessToken = res.data.token.accessToken;
+        let refresh_token = res.data.token.refreshToken.token;
+        let user_id = res.data.user._id;
+        let user_email = res.data.user.email;
+
+        localStorage.setItem("access_token", accessToken);
+        localStorage.setItem("refresh_token", refresh_token);
+        localStorage.setItem("user_id", user_id);
+        localStorage.setItem("user_email", user_email);
+      }
+    })
+    .catch(() => {
+      response = 401;
+    });
   return response;
 };
 
-const checkCredential = async (data) =>{
+const checkCredential = async (data) => {
   await Axios.post(`${apiDomain}/api/${apiVersion}/auth/check-credential`, data)
-  .then((res) =>{
-    if(res.status === 204){
-      response = res.status;
-    }
-  })
-  .catch(() =>{
-    response = 401;
-  })
+    .then((res) => {
+      if (res.status === 204) {
+        response = res.status;
+      }
+    })
+    .catch(() => {
+      response = 401;
+    });
   return response;
 };
 
-const logout = async(localData) =>{
+const logout = async (localData) => {
   try {
     let requestUrl = "logout";
     let data = {
-      access_token : localStorage.getItem('access_token'),
-      refresh_token: localStorage.getItem('refresh_token'),
-      user_email: localStorage.getItem('user_email')
-    }
-    
-    if(localData){
-      requestUrl = "logoutAndRefresh";
+      access_token: localStorage.getItem("access_token"),
+      refresh_token: localStorage.getItem("refresh_token"),
+      user_email: localStorage.getItem("user_email"),
+    };
+
+    if (localData) {
+      requestUrl = "logout-and-refresh";
       data = localData;
     }
 
-    if(!localData){
+    if (!localData) {
       localStorage.clear();
       sessionStorage.clear();
     }
@@ -62,17 +66,16 @@ const logout = async(localData) =>{
       baseURL: apiDomain,
       timeout: 15000,
       headers: {
-        ContentType: 'applications/json',
-        Accept: 'application/json',
-        Authorization: `Bearer ${data.access_token}`
-      }
-    });
-    
-    await logout.post(`${apiDomain}/api/${apiVersion}/auth/${requestUrl}`, {
-      token : data.refresh_token,
-      email : data.user_email
+        ContentType: "applications/json",
+        Accept: "application/json",
+        Authorization: `Bearer ${data.access_token}`,
+      },
     });
 
+    await logout.post(`${apiDomain}/api/${apiVersion}/auth/${requestUrl}`, {
+      token: data.refresh_token,
+      email: data.user_email,
+    });
   } catch (error) {
     console.log(error);
   }
@@ -83,62 +86,62 @@ const createAxiosHeader = () => {
     baseURL: apiDomain,
     timeout: 15000,
     headers: {
-      ContentType: 'applications/json',
-      Accept: 'application/json',
-      Authorization: `Bearer ${localStorage.getItem('access_token')}`
-    }
+      ContentType: "applications/json",
+      Accept: "application/json",
+      Authorization: `Bearer ${localStorage.getItem("access_token")}`,
+    },
   });
-}
+};
 
-const refreshToken = async() =>{
-  const refreshToken = localStorage.getItem('refresh_token');
-  const email = localStorage.getItem('user_email');
-  if(refreshToken && email){
+const refreshToken = async () => {
+  const refreshToken = localStorage.getItem("refresh_token");
+  const email = localStorage.getItem("user_email");
+  if (refreshToken && email) {
     const refresh = createAxiosHeader();
     try {
       await refresh
-      .post(`/api/${apiVersion}/auth/refresh-token`, { 
-        refreshToken,
-        email
-      })
-      .then((response) => {
-        localStorage.setItem('access_token', response.data.accessToken);
-        localStorage.setItem('refresh_token', response.data.refreshToken.token);
-      });
+        .post(`/api/${apiVersion}/auth/refresh-token`, {
+          refreshToken,
+          email,
+        })
+        .then((response) => {
+          localStorage.setItem("access_token", response.data.accessToken);
+          localStorage.setItem(
+            "refresh_token",
+            response.data.refreshToken.token
+          );
+        });
       return true;
     } catch (error) {
       return false;
     }
-   
-  }else{
+  } else {
     return false;
   }
-
 };
 
 const checkAuth = async () => {
-  const refreshToken = localStorage.getItem('refresh_token');
-  const email = localStorage.getItem('user_email');
-  if(refreshToken && email){
+  const refreshToken = localStorage.getItem("refresh_token");
+  const email = localStorage.getItem("user_email");
+  if (refreshToken && email) {
     const checkAuth = createAxiosHeader();
     try {
-      await checkAuth
-      .get(`/api/${apiVersion}/auth/check-token`);
+      await checkAuth.get(`/api/${apiVersion}/auth/check-token`);
       return true;
     } catch (error) {
       return false;
     }
-  }else{
+  } else {
     return false;
   }
-}
+};
 
-const isAuthenticated = async() =>{
-    if(await checkAuth()){
-      authenticated = true;
-    }else{
-      authenticated = false;
-    }
+const isAuthenticated = async () => {
+  if (await checkAuth()) {
+    authenticated = true;
+  } else {
+    authenticated = false;
+  }
 
   return authenticated;
 };
