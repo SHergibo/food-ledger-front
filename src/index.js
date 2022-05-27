ffbf275ddc012044f9a403654779303cefca7657
SHergibo <sachau888@hotmail.com>
2022-05-27 14:38:21 +0200

    feat: Updated index.js for react 18.

--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import { BrowserRouter } from 'react-router-dom';
+import React from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App";
+import { BrowserRouter } from "react-router-dom";
 
-ReactDOM.render(
+const container = document.getElementById("root");
+const root = createRoot(container);
+root.render(
   <BrowserRouter>
     {/* <React.StrictMode> */}
-      <App />
+    <App />
     {/* </React.StrictMode> */}
-  </BrowserRouter>,
-  document.getElementById('root')
+  </BrowserRouter>
 );
