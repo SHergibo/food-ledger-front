e700f8407f0492cedfc21ae0695a1eb805cbcd7e
SHergibo <sachau888@hotmail.com>
2022-05-30 14:55:31 +0200

    feat: Updated NoHousehold component with react-router-dom v6.

--- src/components/PreppersAppComponents/UtilitiesComponent/NoHousehold.js
+++ src/components/PreppersAppComponents/UtilitiesComponent/NoHousehold.js
@@ -1,17 +1,29 @@
-import React  from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { useNavigate } from "react-router-dom";
 
 function NoHousehold() {
+  const navigate = useNavigate();
+  const navigateToHouseholdOption = () => {
+    navigate("/app/options", {
+      state: {
+        householdOptions: true,
+      },
+    });
+  };
+
   return (
     <div className="no-household-container">
       <div className="no-household-content">
-        <p>Il semblerait que vous n'ayez pas de famille, pour pouvoir avoir accès à cette page, il faut d'abord créer ou rejoindre une famille !</p> 
-        <Link className="btn-purple" to="/app/options">
+        <p>
+          Il semblerait que vous n'ayez pas de famille, pour pouvoir avoir accès
+          à cette page, il faut d'abord créer ou rejoindre une famille !
+        </p>
+        <button className="btn-purple" onClick={navigateToHouseholdOption}>
           Créer / rejoindre une famille
-        </Link>
+        </button>
       </div>
     </div>
-  )
+  );
 }
 
-export default NoHousehold
+export default NoHousehold;
