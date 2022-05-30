e78297bac9c5611cdb144d833a62fb77910a4ebc
SHergibo <sachau888@hotmail.com>
2022-05-30 14:47:23 +0200

    fix: Updated DataContext component with react-router-dom v6.

--- src/components/PreppersAppComponents/DataContext.js
+++ src/components/PreppersAppComponents/DataContext.js
@@ -1,7 +1,14 @@
-import React, { useContext, useState, createContext, useEffect, useRef, useCallback } from 'react';
-import { useHistory } from 'react-router-dom';
-import axiosInstance from './../../utils/axiosInstance';
-import { apiDomain, apiVersion } from './../../apiConfig/ApiConfig';
+import React, {
+  useContext,
+  useState,
+  createContext,
+  useEffect,
+  useRef,
+  useCallback,
+} from "react";
+import { useNavigate } from "react-router-dom";
+import axiosInstance from "./../../utils/axiosInstance";
+import { apiDomain, apiVersion } from "./../../apiConfig/ApiConfig";
 import { io } from "socket.io-client";
 
 const UserDataContext = createContext();
@@ -11,32 +18,32 @@ const NotificationContext = createContext();
 const SocketContext = createContext();
 const WindowWidthContext = createContext();
 
-export function useUserData(){
+export function useUserData() {
   return useContext(UserDataContext);
 }
 
-export function useUserHouseHoldData(){
+export function useUserHouseHoldData() {
   return useContext(UserHouseholdDataContext);
 }
 
-export function useUserOptionData(){
+export function useUserOptionData() {
   return useContext(UserOptionContext);
 }
 
-export function useNotificationData(){
+export function useNotificationData() {
   return useContext(NotificationContext);
 }
 
-export function useSocket(){
+export function useSocket() {
   return useContext(SocketContext);
 }
 
-export function useWindowWidth(){
+export function useWindowWidth() {
   return useContext(WindowWidthContext);
 }
 
-export function DataProvider({children}) {
-  const history = useHistory();
+export function DataProvider({ children }) {
+  const navigate = useNavigate();
   const [userData, setUserData] = useState();
   const [userHouseholdData, setUserHouseholdData] = useState();
   const [userOptionData, setUserOptionData] = useState();
@@ -48,25 +55,27 @@ export function DataProvider({children}) {
 
   useEffect(() => {
     const getUserData = async () => {
-      const getUserDataEndPoint = `${apiDomain}/api/${apiVersion}/users/${localStorage.getItem('user_id')}`;
-      await axiosInstance.get(getUserDataEndPoint)
-        .then((response) => {
-          if(isMounted.current){
-            setUserData(response.data);
-            getUserHouseholdData(response.data.householdId);
-            getUserOptionData(response.data._id);
-            fetchNotification(response.data._id);
-          }
-        });
+      const getUserDataEndPoint = `${apiDomain}/api/${apiVersion}/users/${localStorage.getItem(
+        "user_id"
+      )}`;
+      await axiosInstance.get(getUserDataEndPoint).then((response) => {
+        if (isMounted.current) {
+          setUserData(response.data);
+          getUserHouseholdData(response.data.householdId);
+          getUserOptionData(response.data._id);
+          fetchNotification(response.data._id);
+        }
+      });
     };
     getUserData();
 
     const getUserHouseholdData = async (householdId) => {
-      if(householdId !== null){
+      if (householdId !== null) {
         const getUserHouseholdDataEndPoint = `${apiDomain}/api/${apiVersion}/households/${householdId}`;
-        await axiosInstance.get(getUserHouseholdDataEndPoint)
+        await axiosInstance
+          .get(getUserHouseholdDataEndPoint)
           .then((response) => {
-            if(isMounted.current){
+            if (isMounted.current) {
               setUserHouseholdData(response.data);
             }
           });
@@ -75,99 +84,117 @@ export function DataProvider({children}) {
 
     const getUserOptionData = async (userId) => {
       const getUserOptionDataEndPoint = `${apiDomain}/api/${apiVersion}/options/${userId}`;
-      await axiosInstance.get(getUserOptionDataEndPoint)
-        .then((response) => {
-          if(isMounted.current){
-            setUserOptionData(response.data);
-          }
-        });
+      await axiosInstance.get(getUserOptionDataEndPoint).then((response) => {
+        if (isMounted.current) {
+          setUserOptionData(response.data);
+        }
+      });
     };
 
     const fetchNotification = async (userId) => {
       const getNotificationEndPoint = `${apiDomain}/api/${apiVersion}/notifications/received-notification/${userId}`;
-      await axiosInstance.get(getNotificationEndPoint)
-        .then((response) => {
-          if(isMounted.current){
-            setNotificationReceived(response.data);
-          }
-        });
+      await axiosInstance.get(getNotificationEndPoint).then((response) => {
+        if (isMounted.current) {
+          setNotificationReceived(response.data);
+        }
+      });
     };
 
     const connectSocketIo = () => {
       socketRef.current = io(apiDomain);
       socketRef.current.on("connect", () => {
-        socketRef.current.emit('enterSocketRoom', {socketRoomName: localStorage.getItem('user_id')});
+        socketRef.current.emit("enterSocketRoom", {
+          socketRoomName: localStorage.getItem("user_id"),
+        });
       });
 
       socketRef.current.on("refreshData", () => {
-        if(JSON.parse(localStorage.getItem('needRefresh')) === true){
+        if (JSON.parse(localStorage.getItem("needRefresh")) === true) {
           getUserData();
           socketRef.current.disconnect();
           connectSocketIo();
-          localStorage.removeItem('needRefresh');
+          localStorage.removeItem("needRefresh");
         }
       });
-  
+
       socketRef.current.on("updateNotificationReceived", (notif) => {
-        setNotificationReceived(notificationReceived => [...notificationReceived, notif]);
+        setNotificationReceived((notificationReceived) => [
+          ...notificationReceived,
+          notif,
+        ]);
       });
-  
+
       socketRef.current.on("deleteNotificationReceived", (notifId) => {
-        setNotificationReceived(notificationReceived => notificationReceived.filter((notif) => notif._id !== notifId));
+        setNotificationReceived((notificationReceived) =>
+          notificationReceived.filter((notif) => notif._id !== notifId)
+        );
       });
-  
-      socketRef.current.on("updateAllNotificationsReceived", (allNotifReceived) => {
-        setNotificationReceived(allNotifReceived);
-      });
-  
+
+      socketRef.current.on(
+        "updateAllNotificationsReceived",
+        (allNotifReceived) => {
+          setNotificationReceived(allNotifReceived);
+        }
+      );
+
       socketRef.current.on("updateUserAndFamillyData", (data) => {
         setUserData(data.userData);
         setUserHouseholdData(data.householdData);
       });
-  
+
       socketRef.current.on("updateFamilly", (householdData) => {
         setUserHouseholdData(householdData);
       });
 
       socketRef.current.on("logoutSameNavigator", () => {
-        if(!localStorage.getItem('access_token')){
-          history.push('/');
+        if (!localStorage.getItem("access_token")) {
+          navigate("/");
         }
       });
-    }
+    };
 
     connectSocketIo();
 
     return () => {
       socketRef.current.disconnect();
     };
-  }, [history]);
+  }, [navigate]);
 
-
-  const responsiveWidth = useCallback(() =>{
+  const responsiveWidth = useCallback(() => {
     setWindowWidth(window.innerWidth);
   }, []);
 
   useEffect(() => {
-    window.addEventListener('resize', responsiveWidth);
-    return () =>{
-      window.removeEventListener('resize', responsiveWidth);
-    }
+    window.addEventListener("resize", responsiveWidth);
+    return () => {
+      window.removeEventListener("resize", responsiveWidth);
+    };
   }, [responsiveWidth]);
 
   useEffect(() => {
     return () => {
       isMounted.current = false;
-    }
+    };
   }, []);
 
-  return(
+  return (
     <UserDataContext.Provider value={{ userData, setUserData }}>
-      <UserHouseholdDataContext.Provider value={{ userHouseholdData, setUserHouseholdData }}>
-        <UserOptionContext.Provider value={{ userOptionData, setUserOptionData }}>
-          <NotificationContext.Provider value={{ notificationReceived, setNotificationReceived, notificationType, setNotificationType }}>
-            <SocketContext.Provider value={{socketRef}}>
-              <WindowWidthContext.Provider value={{windowWidth}}>
+      <UserHouseholdDataContext.Provider
+        value={{ userHouseholdData, setUserHouseholdData }}
+      >
+        <UserOptionContext.Provider
+          value={{ userOptionData, setUserOptionData }}
+        >
+          <NotificationContext.Provider
+            value={{
+              notificationReceived,
+              setNotificationReceived,
+              notificationType,
+              setNotificationType,
+            }}
+          >
+            <SocketContext.Provider value={{ socketRef }}>
+              <WindowWidthContext.Provider value={{ windowWidth }}>
                 {children}
               </WindowWidthContext.Provider>
             </SocketContext.Provider>
@@ -175,5 +202,5 @@ export function DataProvider({children}) {
         </UserOptionContext.Provider>
       </UserHouseholdDataContext.Provider>
     </UserDataContext.Provider>
-  )
+  );
 }
