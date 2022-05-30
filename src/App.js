fe5843af0ff51c11e4db01e61867aa154f2c8b38
SHergibo <sachau888@hotmail.com>
2022-05-30 14:46:47 +0200

    fix: Updated App component with react-router-dom v6.

--- src/App.js
+++ src/App.js
@@ -1,39 +1,50 @@
-import React from 'react';
-import './styles/Styles.scss';
-import { Route, Switch } from 'react-router-dom';
-import IsLoggedRoute from './components/Route/IsLogged.route';
-import ProtectedRoute from './components/Route/Protected.route';
-import SignInUp from './components/SignInSignUpComponent/SignInUp';
-import PreppersApp from './components/PreppersAppComponents/PreppersApp';
-import Page404 from './components/Page404';
-import { library } from '@fortawesome/fontawesome-svg-core';
-import { 
-  faHome, 
-  faList, 
-  faHistory, 
-  faSignOutAlt, 
+import React from "react";
+import "./styles/Styles.scss";
+import { Route, Routes } from "react-router-dom";
+import IsLoggedRoute from "./components/Route/IsLogged.route";
+import ProtectedRoute from "./components/Route/Protected.route";
+import HasHousehold from "./components/Route/HasHousehold.route";
+import SignInUp from "./components/SignInSignUpComponent/SignInUp";
+import PreppersApp from "./components/PreppersAppComponents/PreppersApp";
+import ProductList from "./components/PreppersAppComponents/SubComponent/ProductList";
+import EditProduct from "./components/PreppersAppComponents/ProductHistoricComponents/EditProduct";
+import AddProduct from "./components/PreppersAppComponents/ProductHistoricComponents/AddProduct";
+import HistoricList from "./components/PreppersAppComponents/SubComponent/HistoricList";
+import Option from "./components/PreppersAppComponents/SubComponent/Option";
+import Statistics from "./components/PreppersAppComponents/SubComponent/Statistics";
+import ProductLog from "./components/PreppersAppComponents/SubComponent/ProductLog";
+import ShoppingList from "./components/PreppersAppComponents/SubComponent/ShoppingList";
+import SubContainer from "./components/PreppersAppComponents/PreppersAppUI/SubContainer";
+import EditBrand from "./components/PreppersAppComponents/SubComponent/OptionComponents/EditBrand";
+import Page404 from "./components/Page404";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import {
+  faHome,
+  faList,
+  faHistory,
+  faSignOutAlt,
   faSignInAlt,
-  faUser, 
-  faUserPlus, 
-  faChartPie, 
-  faBars, 
-  faBell, 
-  faArrowLeft, 
-  faCheck, 
-  faExclamation, 
-  faPlus, 
-  faTimes, 
-  faPen, 
-  faFilter, 
-  faEdit, 
-  faTrash, 
-  faAngleDoubleLeft, 
-  faAngleLeft, 
-  faAngleRight, 
-  faAngleDoubleRight, 
-  faUndo, 
+  faUser,
+  faUserPlus,
+  faChartPie,
+  faBars,
+  faBell,
+  faArrowLeft,
+  faCheck,
+  faExclamation,
+  faPlus,
+  faTimes,
+  faPen,
+  faFilter,
+  faEdit,
+  faTrash,
+  faAngleDoubleLeft,
+  faAngleLeft,
+  faAngleRight,
+  faAngleDoubleRight,
+  faUndo,
   faCog,
-  faChevronLeft ,
+  faChevronLeft,
   faClipboardList,
   faShoppingCart,
   faEnvelope,
@@ -42,34 +53,34 @@ import {
   faDoorOpen,
   faRandom,
   faSearch,
-  faChevronUp
-} from '@fortawesome/free-solid-svg-icons';
+  faChevronUp,
+} from "@fortawesome/free-solid-svg-icons";
 
 library.add(
-  faHome, 
-  faList, 
-  faHistory, 
-  faSignOutAlt, 
-  faSignInAlt, 
-  faUser, 
+  faHome,
+  faList,
+  faHistory,
+  faSignOutAlt,
+  faSignInAlt,
+  faUser,
   faUserPlus,
-  faChartPie, 
-  faBars, 
-  faBell, 
-  faArrowLeft, 
-  faCheck, 
-  faExclamation, 
-  faPlus, 
-  faTimes, 
-  faPen, 
-  faFilter, 
-  faEdit, 
-  faTrash, 
-  faAngleDoubleLeft, 
-  faAngleLeft, 
-  faAngleRight, 
-  faAngleDoubleRight, 
-  faUndo, 
+  faChartPie,
+  faBars,
+  faBell,
+  faArrowLeft,
+  faCheck,
+  faExclamation,
+  faPlus,
+  faTimes,
+  faPen,
+  faFilter,
+  faEdit,
+  faTrash,
+  faAngleDoubleLeft,
+  faAngleLeft,
+  faAngleRight,
+  faAngleDoubleRight,
+  faUndo,
   faCog,
   faChevronLeft,
   faClipboardList,
@@ -86,22 +97,50 @@ library.add(
 function App() {
   return (
     <div className="app">
-      <Switch>
-        <IsLoggedRoute exact path="/" component={SignInUp} />
-        <ProtectedRoute exact path="/app/liste-produit" component={PreppersApp} />
-        <ProtectedRoute exact path="/app/edition-produit/:id" component={PreppersApp} />
-        <ProtectedRoute exact path="/app/ajout-produit" component={PreppersApp} />
-        <ProtectedRoute exact path="/app/liste-historique" component={PreppersApp} />
-        <ProtectedRoute exact path="/app/edition-historique/:id" component={PreppersApp} />
-        <ProtectedRoute exact path="/app/ajout-historique" component={PreppersApp} />
-        <ProtectedRoute exact path="/app/options" component={PreppersApp} />
-        <ProtectedRoute exact path="/app/edition-marque/:id" component={PreppersApp} />
-        <ProtectedRoute exact path="/app/statistiques" component={PreppersApp} />
-        <ProtectedRoute exact path="/app/registre-produit" component={PreppersApp} />
-        <ProtectedRoute exact path="/app/liste-de-course" component={PreppersApp} />
-        <ProtectedRoute exact path="/app/notification" component={PreppersApp} />
-        <Route path="*" component={Page404} />
-      </Switch>
+      <Routes>
+        <Route element={<IsLoggedRoute />}>
+          <Route path="/" element={<SignInUp />} />
+        </Route>
+
+        <Route element={<ProtectedRoute />}>
+          <Route path="app" element={<PreppersApp />}>
+            <Route element={<HasHousehold />}>
+              <Route path="liste-produit" element={<ProductList />} />
+            </Route>
+            <Route element={<HasHousehold />}>
+              <Route path="edition-produit/:id" element={<EditProduct />} />
+            </Route>
+            <Route element={<HasHousehold />}>
+              <Route path="ajout-produit" element={<AddProduct />} />
+            </Route>
+            <Route element={<HasHousehold />}>
+              <Route path="liste-historique" element={<HistoricList />} />
+            </Route>
+            <Route element={<HasHousehold />}>
+              <Route path="edition-historique/:id" element={<EditProduct />} />
+            </Route>
+            <Route element={<HasHousehold />}>
+              <Route path="ajout-historique" element={<AddProduct />} />
+            </Route>
+            <Route element={<HasHousehold />}>
+              <Route path="edition-marque/:id" element={<EditBrand />} />
+            </Route>
+            <Route element={<HasHousehold />}>
+              <Route path="statistiques" element={<Statistics />} />
+            </Route>
+            <Route element={<HasHousehold />}>
+              <Route path="registre-produit" element={<ProductLog />} />
+            </Route>
+            <Route element={<HasHousehold />}>
+              <Route path="liste-de-course" element={<ShoppingList />} />
+            </Route>
+            <Route path="options" element={<Option />} />
+            <Route path="notification" element={<SubContainer />} />
+          </Route>
+        </Route>
+
+        <Route path="*" element={<Page404 />} />
+      </Routes>
     </div>
   );
 }
