58d6f3c40847cbfa3f995e16972482ff431cb6ff
SHergibo <sachau888@hotmail.com>
2022-05-30 14:52:07 +0200

    feat: Updated AlreadyLogged component with react-router-dom v6.

--- src/components/SignInSignUpComponent/AlreadyLogged.js
+++ src/components/SignInSignUpComponent/AlreadyLogged.js
@@ -1,40 +1,45 @@
-import React from 'react';
-import { loginIn, logout } from './../../utils/Auth';
-import PropTypes from 'prop-types';
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import { loginIn, logout } from "./../../utils/Auth";
+import PropTypes from "prop-types";
 
-function AlreadyLogged({history, loginData}) {
+function AlreadyLogged({ loginData }) {
+  const navigate = useNavigate;
   const stayLogged = () => {
-    history.push("/app/liste-produit");
+    navigate("/app/liste-produit");
   };
 
-  const disconnect = async() => {
+  const disconnect = async () => {
     let localStore = {
-      access_token:  localStorage.getItem("access_token"),
-      refresh_token: localStorage.getItem('refresh_token'),
-      user_id: localStorage.getItem('user_id'),
-      user_email: localStorage.getItem('user_email')
-    }
+      access_token: localStorage.getItem("access_token"),
+      refresh_token: localStorage.getItem("refresh_token"),
+      user_id: localStorage.getItem("user_id"),
+      user_email: localStorage.getItem("user_email"),
+    };
     let responseLogin = await loginIn(loginData);
-    localStorage.setItem('needRefresh', true);
+    localStorage.setItem("needRefresh", true);
     await logout(localStore);
     if (responseLogin !== 401) {
-      history.push("/app/liste-produit");
+      navigate("/app/liste-produit");
     }
-  }
+  };
 
   return (
     <div>
       <h1>Login</h1>
-      <p>{`Vous êtes déjà connecté avec l'addresse mail : ${localStorage.getItem('user_email')}.`}</p>
+      <p>{`Vous êtes déjà connecté avec l'addresse mail : ${localStorage.getItem(
+        "user_email"
+      )}.`}</p>
       <button onClick={stayLogged}>Rester connecté avec ce compte.</button>
-      <button onClick={disconnect}>Se déconnecter et se connecter avec un autre compte.</button>
+      <button onClick={disconnect}>
+        Se déconnecter et se connecter avec un autre compte.
+      </button>
     </div>
-  )
+  );
 }
 
 AlreadyLogged.propTypes = {
-  history : PropTypes.object.isRequired,
-  loginData : PropTypes.object.isRequired,
-}
+  loginData: PropTypes.object.isRequired,
+};
 
 export default AlreadyLogged;
