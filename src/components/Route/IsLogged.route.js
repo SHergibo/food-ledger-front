86389ac7c6dd598f18a1e8cba5e1206bf63aa073
SHergibo <sachau888@hotmail.com>
2022-05-30 14:49:56 +0200

    fix: Updated IsLogged route component with react-router-dom v6.

--- src/components/Route/IsLogged.route.js
+++ src/components/Route/IsLogged.route.js
@@ -1,37 +1,57 @@
-import React, { Component } from 'react';
-import { Route, Redirect } from 'react-router-dom';
-import { isAuthenticated } from './../../utils/Auth';
+import React, { useState, useEffect } from "react";
+// import { Route, Redirect } from 'react-router-dom';
+import { Navigate, Outlet } from "react-router-dom";
+import { isAuthenticated } from "./../../utils/Auth";
 
-class RouteRender extends Component {
-  constructor(props) {
-    super(props)
-    this.state = { authorized: null }
-  }
+const IsLoggedRoute = () => {
+  const [logged, setLogged] = useState();
 
-  componentDidMount() {
-    isAuthenticated().then(
-      authorized => this.setState({ authorized})
-    )
-  }
+  useEffect(() => {
+    const checkAuth = async () => {
+      isAuthenticated().then((res) => {
+        setLogged(res);
+      });
+    };
+    checkAuth();
+  }, []);
 
-  render() {
-    if(this.state.authorized === true) {
-      return (<Redirect to={{
-        pathname: '/app/liste-produit',
-        state: { from: this.props.location }
-      }} />)
-    } else if(this.state.authorized === false) {
-      const { component: Component, componentProps } = this.props
-      return <Component {...componentProps} />
-    }
-    return null;
-  }
-}
+  if (logged === undefined) return null;
 
-const ProtectedRoute = function ({ component: Component, ...rest }) {
-  return (
-    <Route {...rest} render={props => <RouteRender componentProps={props} component={Component} />} />
-  )
-}
+  return logged ? <Navigate replace to="/app/liste-produit" /> : <Outlet />;
+};
 
-export default ProtectedRoute;
+export default IsLoggedRoute;
+
+// class RouteRender extends Component {
+//   constructor(props) {
+//     super(props)
+//     this.state = { authorized: null }
+//   }
+
+//   componentDidMount() {
+//     isAuthenticated().then(
+//       authorized => this.setState({ authorized})
+//     )
+//   }
+
+//   render() {
+//     if(this.state.authorized === true) {
+//       return (<Redirect to={{
+//         pathname: '/app/liste-produit',
+//         state: { from: this.props.location }
+//       }} />)
+//     } else if(this.state.authorized === false) {
+//       const { component: Component, componentProps } = this.props
+//       return <Component {...componentProps} />
+//     }
+//     return null;
+//   }
+// }
+
+// const ProtectedRoute = function ({ component: Component, ...rest }) {
+//   return (
+//     <Route {...rest} render={props => <RouteRender componentProps={props} component={Component} />} />
+//   )
+// }
+
+// export default ProtectedRoute;
